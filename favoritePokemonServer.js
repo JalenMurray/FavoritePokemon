29ec16056c1e7d8e9e7540c3817b2673b0ddd233
Jalen Murray <jalenmmurray@gmail.com>
2022-12-16 08:40:12 -0500

    Added Remove and Remove all Functionality

--- favoritePokemonServer.js
+++ favoritePokemonServer.js
@@ -87,21 +87,17 @@ app.post('/select', async (request, response) => {
       favorite: favorite,
    }
    addFavorite(entry).catch(console.error)
-   console.log(favorite)
-   const fetchResponse = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${favorite}`
-   )
-   const pokemon = await fetchResponse.json()
+   const info = await getInfo(favorite)
    let variables = {
       header: `Thanks ${cap(
          name
       )} for Selecting your Favorite Pokemon from Gen 1!  Here is some info on ${cap(
          favorite
       )}`,
-      id: pokemon.id,
-      name: cap(pokemon.name),
-      types: pokemon.types.map((type) => cap(type.type.name)),
-      url: `https://pokeapi.co/api/v2/pokemon/${favorite}`,
+      id: info.id,
+      name: info.name,
+      types: info.types,
+      url: info.url,
    }
    response.render('info', variables)
 })
@@ -113,20 +109,75 @@ app.get('/find', (request, response) => {
 app.post('/find', async (request, response) => {
    let { name } = request.body
    let favorite = await getFavorite(name)
-   const fetchResponse = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${favorite.favorite}`
-   )
-   const pokemon = await fetchResponse.json()
-   let variables = {
-      header: `${cap(favorite.name)}'s Favorite Gen 1 Pokemon is ${cap(
-         favorite.favorite
-      )}! Here is some info on ${cap(favorite.favorite)}`,
-      id: pokemon.id,
-      name: cap(pokemon.name),
-      types: pokemon.types.map((type) => cap(type.type.name)),
-      url: `https://pokeapi.co/api/v2/pokemon/${favorite.favorite}`,
+   let variables
+   if (favorite) {
+      const info = await getInfo(favorite.favorite)
+      variables = {
+         header: `${cap(favorite.name)}'s Favorite Gen 1 Pokemon is ${cap(
+            favorite.favorite
+         )}! Here is some info on ${cap(favorite.favorite)}`,
+         id: info.id,
+         name: info.name,
+         types: info.types,
+         url: info.url,
+      }
+   } else {
+      variables = {
+         header: `${cap(name)} Does not Have an Entry in the Database`,
+         id: 'NONE',
+         name: 'NONE',
+         types: 'NONE',
+         url: `NONE`,
+      }
    }
+
    response.render('info', variables)
+})
+
+app.get('/findNames', (request, response) => {
+   response.render('findNames', {})
+})
+app.post('/findNames', async (request, response) => {
+   let { pokemonName } = request.body
+   let people = await getNames(pokemonName)
+   let peopleStr = ''
+   people.forEach((entry) => (peopleStr += `<li>${entry.name}</li>`))
+   const variables = {
+      pokemonName: cap(pokemonName),
+      people: peopleStr,
+   }
+   response.render('people', variables)
+})
+
+app.get('/remove', (request, response) => {
+   response.render('remove', {})
+})
+app.post('/remove', async (request, response) => {
+   let { name } = request.body
+   let variables
+   if (!name) {
+      variables = {
+         msg: 'No Entries were removed\n\nPlease provide a valid name',
+      }
+   } else {
+      removeEntry(name)
+      variables = {
+         msg: `Entry for ${name} has been removed`,
+      }
+   }
+
+   response.render('removeConfirm', variables)
+})
+
+app.get('/removeAll', (request, response) => {
+   response.render('removeAll', {})
+})
+app.post('/removeAll', async (request, response) => {
+   removeEntry()
+   const variables = {
+      msg: `All Entries have been removed`,
+   }
+   response.render('removeConfirm', variables)
 })
 
 // MongoDB Helper Funcs
@@ -161,14 +212,11 @@ async function getFavorite(name) {
    }
 }
 
-async function getManyApplications(client, database, collection, minGPA) {
-   let filter = { gpa: { $gte: minGPA } }
+async function getNames(pokemonName) {
+   let filter = { favorite: pokemonName.toLowerCase() }
    try {
       await client.connect()
-      const cursor = await client
-         .db(database)
-         .collection(collection)
-         .find(filter)
+      const cursor = await client.db(db).collection(collection).find(filter)
       let applications = await cursor.toArray()
       return applications
    } catch (e) {
@@ -178,13 +226,14 @@ async function getManyApplications(client, database, collection, minGPA) {
    }
 }
 
-async function removeAllApplications(client, database, collection) {
+async function removeEntry(name) {
    try {
+      let filter = name ? { name: name } : {}
       await client.connect()
       const result = await client
-         .db(database)
+         .db(db)
          .collection(collection)
-         .deleteMany({})
+         .deleteMany(filter)
       return result.deletedCount
    } catch (e) {
       console.error(e)
@@ -219,4 +268,45 @@ async function getPokemonList() {
 
 function cap(name) {
    return name.charAt(0).toUpperCase() + name.slice(1)
+}
+
+async function getInfo(name) {
+   const fetchResponse = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${name}`
+   )
+   const pokemon = await fetchResponse.json()
+   const info = {
+      id: pokemon.id,
+      name: cap(pokemon.name),
+      types: pokemon.types.map(
+         (type) =>
+            `<span style="color: ${getTypeColor(type.type.name)}">${cap(
+               type.type.name
+            )}</span>`
+      ),
+      url: `https://pokeapi.co/api/v2/pokemon/${name}`,
+   }
+   return info
+}
+
+function getTypeColor(type) {
+   if (type == 'bug') return 'rgb(138,150,11)'
+   if (type == 'dark') return 'rgb(62,45,34)'
+   if (type == 'dragon') return 'rgb(111,88,212)'
+   if (type == 'electric') return 'rgb(226,160,27)'
+   if (type == 'fairy') return 'rgb(218,149,215)'
+   if (type == 'fighting') return 'rgb(105,39,25)'
+   if (type == 'fire') return 'rgb(207,38,3)'
+   if (type == 'flying') return 'rgb(144,166,240)'
+   if (type == 'ghost') return 'rgb(52,51,115)'
+   if (type == 'grass') return 'rgb(102,187,42)'
+   if (type == 'ground') return 'rgb(210,178,86)'
+   if (type == 'ice') return 'rgb(115,211,247)'
+   if (type == 'normal') return 'rgb(160,151,140)'
+   if (type == 'poison') return 'rgb(106,53,106)'
+   if (type == 'psychic') return 'rgb(225,49,107)'
+   if (type == 'rock') return 'rgb(160,137,58)'
+   if (type == 'steel') return 'rgb(142,143,156)'
+   if (type == 'water') return 'rgb(7,104,197)'
+   return 'rgb(0, 0, 0)'
 }
